c661e28746422dc8d1fdbe43571fd54edd71ab8e
julio-cesarferrai <juliocesarferrari7@gmail.com>
2024-06-05 20:45:38 -0300

    login ok
    fiz o login atuar sem botão e com mensagem de acesso

--- js/script.js
+++ js/script.js
@@ -1,18 +1,42 @@
 // script.js
-document.getElementById('loginForm').addEventListener('click', acessa);
+    const usuario = document.getElementById('username');
+    const senha = document.getElementById('password');
     
+    // Adiciona ouvintes de eventos para os campos de entrada
+    usuario.addEventListener('input', acessa);
+    senha.addEventListener('input', acessa);
     
-    function acessa() {
+function acessa() {
+    let usernameInput = document.getElementById('username');
+    let passwordInput = document.getElementById('password');
+    let username = usernameInput.value;
+    let password = passwordInput.value;
+    let tamanhosenha= password.length;
+    if (username && password.length>8) 
+        {
+           
+            
+            if (username =="anhaguera" && password=="anhaguera")
+                {
+                    message.style.display = 'block'; // Mostra a mensagem
+                    setTimeout(function() 
+                    {
+                        window.location.href = "./home.html";
+                    }, 2000);
+
+                }
+            else{
+                alert("Usuario ou senha incorreto");
+        }    
+    } else {
+        console.error("Elemento não encontrado");
+    }
 
     
-        var username = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
-
-    
-        if (username == "anhaguera" && password === "anhaguera") {
+       /* if (username == "anhaguera" && password === "anhaguera") {
                 location.href = "./home.html"; // Redireciona para outra página
             }
-    
-    }        
+    */
+    }       
 
 
